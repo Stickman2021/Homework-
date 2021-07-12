65a86764a19f4851b9a94d2f0be7720665ddc4cd
Stickman2021 <85092121+Stickman2021@users.noreply.github.com>
2021-07-11 22:01:13 -0400

    Update index (1).js

--- index (1).js
+++ index (1).js
@@ -5,67 +5,68 @@ const Ucfirst = (str) => {
   console.log(usFirst('rose')); rose
   console.log(usFirst('sarah')); sarah
 
-  const checkspam = (str) => {
+  const checkSpam = (str) => {
     const spam = ['sex', 'free', 'porn', 'viagra']
-    const arr = str.split('');
-    console.log(arr); ['buy', 'viagra', 'now']
-    for (let i = 0; i <arr.length; i++) {
-      const wordtocheck = arr[i];
-      spam.foreach((spamword) => {
-        if(spamwork.tolowercase() === wordtocheck.tolowercase()) {
+    const listOfWordToCheck = str.split(' ');
+    console.log(listOfWordToCheck); ['buy', 'viagra', 'now']
+    for (let i = 0; i <listOfWordsToCheck.length; i++) {
+      const Wordtocheck = listOfWordsToCheck[i];
+      spam.forEach((Spamword) => {
+        ifSpamwork.tolowercase() === Wordtocheck.tolowercase(); {
           return true;
         }
       });
     }
-    return false;
-    checkspam('buy', 'VIAgRa', 'now'); false
+  return false;
+  };
+    console.log(checkSpam('buy', 'VIAgRa', 'now')); false
+
     const truncate = (str, maxlength) => {
   if (str.length > maxlength) {
     return str.slice(0, maxlength) + '...';
   }
   return str;
 };
-console.log(truncate('hello world', 20)); hello world
 
-const extractcurrencyvalue = (str) => {
+const extractCurrencyValue = (str) => {
 return +str.slice(1);
 };
-console.log(typeof extractcurrencyvalue('$1000')); number
+console.log(typeof extractCurrencyValue('$1000')); number
 
 
-const fruits = ['apple', 'banana', 'orange'];
-const Shoppingcart = Fruits;
-Shoppingcart.push('pear');
+const fruits = ['Apple', 'Banana', 'Orange'];
+const shoppingcart = fruits;
+shoppingcart.push('pear');
 
-console.log(Fruits.length); 3
-console.log(Shoppingcart.length); 4
+console.log(fruits.length); 3
+console.log(shoppingcart.length); 4
 
-    const styles = ['jazz', 'blues']
-    styles.push = ['rock-n-roll'];
-    const index = math.floor(styles.length / 2);
-    styles[index] = 'classics';
-    console.log(Styles.splice(0, 1)) jazz
+    const styles = ['Jazz', 'Blues']
+    styles.push = ['Rock-n-Roll'];
+    const index = Math.floor(styles.length / 2);
+    styles[index] = 'Classics';
+    console.log(Styles.splice(0, 1)); Jazz
     styles.unshift(Rap);
     styles.unshift(Raggae);
     
-    console.log(styles); ['Rap', 'Raggae', 'Classics', 'Rock-n-roll']
+    console.log(styles); ['Rap', 'Raggae', 'Classics', 'Rock-n-Roll']
 
     const camelize = (str) => {
       const arr = str.split('-');
 console.log(arr); ['backround','color'], ['list', 'style', 'image'], ['font', 'size']
 let result = arr(0);
     for (let i = 1; i <arr.length; i++) {
-      result = result + Ucfirst(arr[i]);
+      result = result + ucFirst(arr[i]);
     }
 return result;
     };
 
-    console.log(camelize('backround-color')); backroundcolor
-    console.log(camelize('list-style-image')); liststyleimage
-    console.log(camelize('font-size')); fontsize
+    console.log(camelize('backround-color')); backroundColor
+    console.log(camelize('list-style-image')); listStyleImage
+    console.log(camelize('font-size')); fontSize
 
     const filterrange = (arr, a, ab) => (
-      const result = [];
+      
 
 arr.foreach((value) => {
   if (value <= a && value <= b) {
